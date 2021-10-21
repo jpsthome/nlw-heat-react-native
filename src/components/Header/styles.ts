8f16781b73a190394bec8ef1797406b345752884
Joao Pedro <joao.thome@unila.edu.br>
2021-10-20 21:14:49 -0300

    arrumando header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,18 @@
+import { FONTS } from "./../../theme/fonts";
+import { StyleSheet } from "react-native";
+import { COLORS } from "../../theme";
+
+export const styles = StyleSheet.create({
+	container: {
+		width: "100%",
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+		paddingHorizontal: 20,
+	},
+	logoutText: {
+		fontSize: 15,
+		fontFamily: FONTS.REGULAR,
+		color: COLORS.WHITE,
+	},
+});
