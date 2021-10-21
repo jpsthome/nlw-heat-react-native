0057c09961204b4bcdef5a8070ffdf790c138db8
Joao Pedro <joao.thome@unila.edu.br>
2021-10-21 20:41:54 -0300

    finalizando button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
-import { ColorValue, Text, TouchableOpacity, TouchableOpacityProps } from 'react-native'
+import { 
+	ColorValue, 
+	Text, 
+	TouchableOpacity, 
+	TouchableOpacityProps,
+	ActivityIndicator
+} from 'react-native';
 
+import { AntDesign } from '@expo/vector-icons'
 
 import { styles } from './styles';
 
@@ -8,17 +15,34 @@ type Props = TouchableOpacityProps & {
 	title: string;
 	color: ColorValue;
 	backgroundColor: ColorValue;
+	icon?: React.ComponentProps<typeof AntDesign>['name'];
+	isLoading?: boolean;
 }
 
-export function Button({title, color, backgroundColor, ...rest}: Props){
+export function Button({
+	title,
+	color,
+	backgroundColor,
+	icon,
+	isLoading = false,
+	...rest
+}: Props){
 	return (
 		<TouchableOpacity
 			style={[styles.button, { backgroundColor }]}
+			activeOpacity={0.7}
+			disabled={isLoading}
 			{...rest}
 		>
-			<Text style={[styles.title, { color}]}>
-				{title}
-			</Text>
+			{
+				isLoading ? <ActivityIndicator color={color}/> :
+			<>
+				<AntDesign name={icon} size={24} style={styles.icon}/>
+				<Text style={[styles.title, { color}]}>
+					{title}
+				</Text>
+			</>
+			}
 		</TouchableOpacity>
 	);
 }
