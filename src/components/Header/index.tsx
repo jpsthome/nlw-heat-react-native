53f2f8f717cd762f21295b83c4d6f8a84f15acda
Joao Pedro <joao.thome@unila.edu.br>
2021-10-21 22:22:23 -0300

    adicionando authenticacao com a API ao app

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,17 +7,21 @@ import { UserPhoto } from '../UserPhoto'
 import { styles } from "./styles";
 
 import LogoSvg from '../../assets/logo.svg';
+import { useAuth } from "../../hooks/auth";
 
 export function Header() {
+	const { user, signOut } = useAuth();
 	return (
 		<View style={styles.container}>
 			<LogoSvg />
 
 			<View style={styles.logoutButton}>
-				<TouchableOpacity>
-					<Text style={styles.logoutText}>Sair</Text>
-				</TouchableOpacity>
-				<UserPhoto imageUri="https://github.com/jpsthome.png"/>
+				{user && 
+					<TouchableOpacity onPress={signOut}>
+						<Text style={styles.logoutText}>Sair</Text>
+					</TouchableOpacity>
+				}
+				<UserPhoto imageUri={user?.avatar_url}/>
 			</View>
 		</View>
 	);
